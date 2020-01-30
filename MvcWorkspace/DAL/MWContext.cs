18a93a6b3ec79e214fd46052a1e071bf20717697
Jesse Schwarz <jesseschwarz0@gmail.com>
2020-01-29 18:44:54 -0600

    Added Repository and Unit of Work
    Sourced from https://docs.microsoft.com/en-us/aspnet/mvc/overview/older-versions/getting-started-with-ef-5-using-mvc-4/implementing-the-repository-and-unit-of-work-patterns-in-an-asp-net-mvc-application#the-repository-and-unit-of-work-patterns
    Not as good as the Digikey implementation

---
+++ MvcWorkspace/DAL/MWContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.DAL
+{
+    public class MWContext : DbContext
+    {
+        //todo: Fill in dbsets
+        //public DbSet<TType> name { get; set; }
+    }
+}
---
+++ MvcWorkspace/DAL/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ContosoUniversity.DAL
+{
+    public class UnitOfWork : IDisposable
+    {
+        private MWContext context = new MWContext();
+        //todo: Fill this in with repositories
+       /* private GenericRepository<TType> Repository;
+
+        public GenericRepository<TType> Repository
+        {
+            get
+            {
+
+                if (this.departmentRepository == null)
+                {
+                    this.departmentRepository = new GenericRepository<Department>(context);
+                }
+                return departmentRepository;
+            }
+        }
+        */
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
