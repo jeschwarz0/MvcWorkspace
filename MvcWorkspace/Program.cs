c0bb5b3dafdb4a7bd7da30d511a326b29b8bc508
Jesse Schwarz <jesseschwarz0@gmail.com>
2020-09-23 16:26:36 -0500

    Added Unit Tests
     - Set InternalsVisibleTo
     - Removed unused usings in Program.cs

--- MvcWorkspace/Program.cs
+++ MvcWorkspace/Program.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleToAttribute("MvcWorkspace.Tests")]
 
 namespace MvcWorkspace
 {
